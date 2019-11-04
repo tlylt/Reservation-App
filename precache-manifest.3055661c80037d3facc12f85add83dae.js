ef536b3149bb77905a9e08b9522b70873944fa3c
tlylt <liu_yongliang@hotmail.com>
2019-11-04 21:11:27 +0800

    Updates

---
+++ precache-manifest.3055661c80037d3facc12f85add83dae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5202f63a5e99b353c58d5ce43a8de48e",
+    "url": "/Reservation-App/index.html"
+  },
+  {
+    "revision": "482ea98473d4e3f5d8fd",
+    "url": "/Reservation-App/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "6a82a52f915e9b8e75b8",
+    "url": "/Reservation-App/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "482ea98473d4e3f5d8fd",
+    "url": "/Reservation-App/static/js/2.db505384.chunk.js"
+  },
+  {
+    "revision": "6a82a52f915e9b8e75b8",
+    "url": "/Reservation-App/static/js/main.3508755d.chunk.js"
+  },
+  {
+    "revision": "3bb86f97557cb0cf4d45",
+    "url": "/Reservation-App/static/js/runtime-main.5fe33877.js"
+  }
+]);
